c3f8de15b56b69432a7f5a88b6372a6bcff1d4ad
Jose A. Lemus <jlemus1234@gmail.com>
2018-12-01 12:18:11 -0500

    messages seem to be the way to update while multithreading

---
+++ Erlang/ClientTrials/scrabble.erl
@@ -0,0 +1,112 @@
+%%%
+%%% File	: scrabble.erl
+%%% Author	: Jose Lemus
+%%% Description : A scrabble server that lets you host a four player gam
+%%% November 29, 2018
+%%%--------------
+-module(scrabble).
+-behaviour(gen_server).
+
+%%---------------
+% imports
+%%---------------
+%% client exports
+-export([join_game/1, send_messages/1, send_messages/2, get_messages/1, printMoveDump/1]).
+
+%% server exports
+-export([gmTest/1]).
+-export([sendMoveResult/1, printMoveResult/1, gotNewMove/0]).
+
+
+%%---------------
+%% Server
+%%---------------
+
+sendMoveResult(MoveResult) -> 
+	io:format("~s~n", ["got move result"]),
+	ok.
+	%io:format("~p~n", MoveResult).
+	%printMoveResult(MoveResult).
+
+
+printMoveResult({Result, Board, Scores, {OldTiles, NewTiles}}) ->
+	io:format("~s~n", ["Printing move result"]),
+	io:format("~p~n", [Result]),
+	io:format("~p~n", [Board]),
+	io:format("~p~n", [Scores]),
+	io:format("~p~n", [OldTiles]),
+	io:format("~p~n", [NewTiles]).
+
+loop(Pypid) ->
+	io:format("~s~n", ["looping"]),
+%	flush(),
+	receive 
+		Something ->
+			io:format("~s~n", ["got something"]),
+			io:format("~p~n", [Something])
+	end,
+	io:format("~s~n", ["fin looped"]),
+	python:cast(Pypid, update), % This would be being sent from another erlang process
+
+	loop(Pypid).
+
+
+gmTest(NodeName) -> 
+	{ok, Pypid} = python:start([{python_path, "."}]), % Create python node
+	%Receiver = spawn_link(scrabble, get_messages, [Pypid]),
+	python:call(Pypid, pythonClient, register_handler, [self()]),
+	%python:call(Pypid, pythonClient, startServer, []),
+	python:call(Pypid, pythonClient, startServer, [self(), NodeName]),
+	%timer:sleep(1000),
+	python:cast(Pypid, update), % This would be being sent from another erlang process
+	loop(Pypid),
+	io:format("~s~n", ["finished gmTest"]).
+	%python:call(Pypid, pythonClient, updateStateTest, []).
+	%timer:sleep(1000),
+	%python:call(Pypid, pythonClient, updateStateTest, []).
+
+
+% simple test function for sending python data
+gotNewMove()->
+	io:format("~s~n", ["Got new move"]).
+
+%%---------------
+%%% Client functions
+%%---------------
+
+join_game(NodeName) -> 
+	{ok, Pypid} = python:start([{python_path, "."}]), % Create python node
+	Receiver = spawn_link(scrabble, get_messages, [Pypid]),
+	python:call(Pypid, pythonClient, runGame, [self(), NodeName]).
+	
+
+
+
+get_messages(Pypid) ->
+	receive {message, MessageText} ->
+		io:format("~s~n", [MessageText]);
+	_X -> io:format("~w~n", [something])
+	end,
+	get_messages(Pypid).
+
+
+%% might be useful for processing input
+%% Send your shit to this function
+%%processInput()
+
+send_messages(ServerPID) ->
+	io:format("~w~n", [something]),
+	io:format("~s~n", [ServerPID]),
+	1.
+
+send_messages(ServerPID, MoveDump) ->
+	io:format("~w~n", [something2]),
+	io:format("~p~n", [MoveDump]),
+	printMoveDump(MoveDump).
+
+
+printMoveDump({TileArr, Dir, Start_pos}) ->
+	io:format("~s~n", ["printMoveDump"]),
+	io:format("~p~n", [TileArr]),
+	io:format("~p~n", [Dir]),
+	io:format("~p~n", [Start_pos]).
