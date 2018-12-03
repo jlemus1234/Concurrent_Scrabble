c24507a41c756acd622ffe4589ca888b8e9c9045
Billy Witrock <william.witrock@tufts.edu>
2018-12-03 16:17:41 -0500

    started to word on middle for player

--- middle_for_player.py
+++ middle_for_player.py
@@ -4,16 +4,23 @@
 from player import Player
 from gui import Gui
 
-player
-gui
+global player
+global gui
+global PID_my
+global PID_server
 
-def start(Pid):
+
+def start(my_Pid, server_Pid):
+    global player, gui, Pid_my, PID_server
     player = Player(Pid)
     gui = Gui()
     player.getGUI(gui)
     gui.getPlayer(player)
     # something like this
     gui.start()
+    PID_my = my_Pid
+    PID_server = server_Pid
+    # need to now send message to server registering me
 
 
 def recieve_message():
