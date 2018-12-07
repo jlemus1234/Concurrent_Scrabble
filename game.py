99d98710dc6fbac99fcc11550e038285baa6ae8f
Billy Witrock <william.witrock@tufts.edu>
2018-12-07 15:29:36 -0500

    fixed tile distrubution at start

--- game.py
+++ game.py
@@ -76,7 +76,7 @@ class Game:
     def start_game(self):
         for i in range(4):
             tiles = self.bag.take_n_from_bag(7)
-            self.send_to_one_player("tiles", i, True, [[]], [], tiles,tiles)
+            self.send_to_one_player("tiles", i, True, [[]], [], [], tiles)
             self.send_to_one_player("refresh", i, True, self.board.get_board(), [0,0,0,0],[],[])
 
     #
