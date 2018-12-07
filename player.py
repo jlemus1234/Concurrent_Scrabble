8b82064747c63d01a3307f31ff81828e49b08182
Saad Mazhar <saadmazhar15@gmail.com>
2018-12-06 19:57:00 -0500

    Player can now parse for word and connect to middle module

--- player.py
+++ player.py
@@ -11,74 +11,120 @@
 import board
 import tile
 import threading
+from middle_for_player import send_message
+
+NUM_PLAYER = 4
 
 
 class Player:
-    def __init__(self, name, PID):
+    def __init__(self, name=""):
         self.board = board.Board()
-        self.score = 0
-        self.name = "Jane Doe"
+        self.score = [0 for i in range(NUM_PLAYER)]
+        self.name = name
         self.tiles = [tile.Tile() for _ in range(7)]
-        self.erlangPID = PID
         self.gui = None
         self.lock = threading.RLock()
 
-    def getGUI(self, GUI):
-        self.gui = GUI
+    def setGUI(self, GUI):
+        with self.lock:
+            self.gui = GUI
+
+    def refresh_failure(self, board_array, score_ray):
+        with self.lock:
+            self.board.set_board(board_array)
+            self.score = score_ray
+
+        # Convert board to 2d Array of tiles, hand (array of tiles), and score
+        self.gui.refresh(self.board.get_board(), self.tiles, self.score)
+
+    def refresh_success(self, board_array, score_ray, tile_ray):
+        with self.lock:
+            self.board.set_board(board_array)
+            self.score = score_ray
+            for tile in tile_ray:
+                self.tiles.append(tile)
+
+        # Convert board to 2d Array of tiles, hand (array of tiles), and score
+        self.gui.refresh(self.board.get_board(), self.tiles, self.score)
+
+    def get_word_from_line(self, start_pos, direction):
+        with self.lock:
+            row = orig_row = start_pos[0]
+            col = orig_col = start_pos[1]
+            # Add first letter of word to string
+            word = self.board.grid[row][col]
+
+            start_row = -1
+            start_col = -1
+
+            # If word is in row, we will be sliding the col index
+            if direction == "r":
+                start_row = row
+                # Finds begining of word in the current row
+                while self.board.inbounds(col - 1):
+                    if self.board.grid[row][col - 1].is_blank():
+                        start_col = col
+                        break
+                    else:
+                        word.append(self.board.grid[row][col - 1])
+                        
+
+                word.reverse()
+                col = orig_col
+
+                # finds end of word in the current row
+                while self.inbounds(col + 1):
+                    if self.board.grid[row][col + 1].is_blank():
+                        break
+                    else:
+                        word.append(self.board.grid[row][col + 1])
+
+            # If word is in column, we will be sliding the row index
+            else:
+                start_col = col
+                # Finds begining of word in the current col
+                while self.board.inbounds(row - 1):
+                    if self.board.grid[row - 1][col].is_blank():
+                        start_row = row
+                        break
+                    else:
+                        word.append(self.board.grid[row - 1][col])
+                        
+
+                word.reverse()
+                row = orig_row
+
+                # finds end of word in the current col
+                while self.inbounds(row + 1):
+                    if self.board.grid[row + 1][col].is_blank():
+                        break
+                    else:
+                        word.append(self.board.grid[row + 1][col])
+
+            return (word, start_row, start_col)
 
 
     def made_move(self, tile_ray, direction, start_pos, used_tiles):
-        valid = True
+        with self.lock:
+            valid = True
 
-        # Determine if space found in word. If found, boolean set to False
-        for tile in tile_ray:
-            if tile.value == '':
-                valid = False
+            # Determine if space found in word. If found, boolean set to False
+            for tile in tile_ray:
+                if tile.value == '':
+                    valid = False
 
-        # Remember to 
-
-        # If tiles places are all sequential ...
-        if valid:
-            # Board class takes in different values for direction. "d" for row
-            direction = "d" if direction == "r" else "nd"
-            # Update board with tiles and 
-            status, new_grid, new_score = self.board.update(start_pos, 
-                                                        tile_ray, direction)
-            if status == True:
-                # Send message to Erlport
-                status = True
-                # After message received form Erlport ...
-                # Remove tiles used and update to new if successful. Update grid
-
-                # self.board.grid = new_grid
-                # self.score = new_score
+            # If tiles places are all sequential ...
+            if valid:
+                # Get word from entire row or col depending on direction
+                word, new_pos = get_word_from_line(start_pos, direction)
+                # Parse for word and add to board for verification
+                status, new_grid, new_score = self.board.update(new_pos, word
+                                                                direction)
+                if status == True:
+                    # Send message to Erlport
+                    send_message(("move", word, direction, start_pos, used_tiles))
 
 
-    # def sendMessageToErlang(self, tile_ray, direction, start_pos, used_tiles):
-    # def receiveMessageFromErlang(self):
-    # def moveSuccessful()
-
-
-
-
-
-
-
-
-
-
-
-
-
-    # Validate to get start_pos of word, not first tile put down, and the 
-    # array of tiles to represent word
-
-    # import middle module and send stuff to it via function call. Convert 
-    # everything to tuples
-
-    # Have a function to get new board and new score, then call GUI update
 
     # Have function to call GUI fucntion to report winner
-
-    # For each class written, convert to Tuuple (to_tuple)
 
